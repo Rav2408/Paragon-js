1d555866b3a4f3ed7ed4b7f982a5435aa7d42aa2
rciup <rciupek0@gmail.com>
2022-11-27 19:28:29 +0100

    model paragonu

--- models.js
+++ models.js
@@ -18,3 +18,33 @@ class Product{
     }
 }
 
+
+class Receipt{
+    constructor(){
+        this.products = new Array()
+    }
+
+    add(name,amount,price){
+        this.products.push = new Product(name,amount,price)
+    }
+    get(id){
+        return this.products[id]
+    }
+    getAll(){
+        
+    }
+    edit(){
+        
+    }
+    editOrder(product,orderId){
+        //retrunOrderOfObject(product)
+        this.products.splice(orderId,0,product)
+    }
+    delete(){
+        
+    }
+    sequenceGenerator() {
+        return ++idSequnce
+    }
+
+}
